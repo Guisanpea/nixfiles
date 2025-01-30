0ccae08f2693ff634cd988c7a2d36f18fbbe3173
Santi <guisanpea@gmail.com>
2025-01-30 12:50:42 +0100

    Commit aider attempt

--- linux/packages.nix
+++ linux/packages.nix
@@ -1,39 +1,57 @@
-# Linux-specific packages
 { pkgs, ... }:
 
-let
-  # VSCode insiders with specific configuration
-  # Documentation: https://nixos.wiki/wiki/Visual_Studio_Code
-  vscode-insiders = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
-    src = (builtins.fetchTarball {
-      url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-      sha256 = "09j6h0h4lvsh05h92fg66jc4dpfd5nx2mrbs8srdwxpks5gz5s3d";
+
+with pkgs;
+let vscode-insiders =
+    (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
+      src = (builtins.fetchTarball {
+        url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+        sha256 = "09j6h0h4lvsh05h92fg66jc4dpfd5nx2mrbs8srdwxpks5gz5s3d";
+      });
+      version = "latest";
     });
-    version = "latest";
-  });
-in 
-{
-  home.packages = with pkgs; [
-    # Audio
+in {
+  home.packages = [
+    # AUDIO
     pavucontrol
     playerctl
 
-    # Desktop applications
+    # CLI
+    bottom
+    helm
+    bind
+
+    # DESKTOP
     blueman
     chromium
+    clapper
+    discord
     firefox-wayland
     gnome.file-roller
+    gparted
+    qbittorrent
     spotify
+    vlc
     vscode-insiders
-    
-    # Wayland utilities
+    teamviewer
+
+    zathura
+
+    # GAMES
+    jdk17
+    lutris
+    prismlauncher
+    wine
+
+    # WAYLAND
     grim
     slurp
     swappy
     waybar
     wdisplays
+    wmname
 
-    # Development
+    # DESKTOP DEVELOPMENT
     insomnia
     jetbrains.idea-community
   ];
