90434de6d679467283547200b2be1add9e1b4116
Santi <guisanpea@gmail.com>
2025-02-10 11:25:14 +0100

    Minor changes

--- nixie/packages/cli.nix
+++ nixie/packages/cli.nix
@@ -8,13 +8,14 @@ let
       memory_limit = 8G
       max_execution_time = 600
       [xdebug]
-      xdebug.mode = profile
+      xdebug.mode = debug
       xdebug.profiler_output_name = cachegrind.out.%p
+      xdebug.output_dir=/Users/ssanchez/work/ouiteamserver_interne
       xdebug.start_with_request = trigger
       xdebug.discover_client_host = 1
       xdebug.client_host=127.0.0.1
-      xdebug.log=/var/www/var/log/xdebug.log
-      xdebug.log_level=0
+      xdebug.log=/tmp/xdebug.log
+      xdebug.log_level=3
     '';
   };
 in
@@ -76,7 +77,7 @@ in
     kubectl
     kubectx
     leiningen
-    mysql
+    mariadb
     nixfmt-rfc-style
     nodePackages.npm
     nodejs
