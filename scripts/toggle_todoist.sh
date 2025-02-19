8a5ee1b7918b7378fd51d2e88967a10b074847ca
Santi <guisanpea@gmail.com>
2025-02-19 18:42:48 +0100

    Added toggles for todoist and whatsapp

--- scripts/toggle_todoist.sh
+++ scripts/toggle_todoist.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-# Define the app_id for Todoist
-APP_ID="FFPWA-01HKN7B79GP8PQ9ERFFX4RZJ8P"
-
-# Get the window information using swaymsg and jq
-WINDOW_INFO=$(swaymsg -t get_tree | jq '.. | select(.app_id? == "'$APP_ID'")')
-
-# Check if the window exists
-if [[ ! -z "$WINDOW_INFO" ]]; then
-    # Check if the window is visible
-    if [[ $(echo "$WINDOW_INFO" | jq '.visible') == "true" ]]; then
-        # If visible, hide the window
-        swaymsg "[app_id=\"$APP_ID\"] move scratchpad"
-    else
-        # If not visible, show the window
-        swaymsg "[app_id=\"$APP_ID\"] scratchpad show"
-    fi
+if pgrep -f "firefoxpwa.*01HKN7B79GP8PQ9ERFFX4RZJ8P"; then
+    hyprctl dispatch togglespecialworkspace todoist
 else
-   firefoxpwa site launch 01HKN7B79GP8PQ9ERFFX4RZJ8P
+    firefoxpwa site launch 01HKN7B79GP8PQ9ERFFX4RZJ8P
 fi
--- scripts/toggle_whatsapp.sh
+++ scripts/toggle_whatsapp.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-# Define the app_id for Whatsapp
-APP_ID="FFPWA-01HKQ02CZ0FJ90985QX9J9YGEK"
-
-# Get the window information using swaymsg and jq
-WINDOW_INFO=$(swaymsg -t get_tree | jq '.. | select(.app_id? == "'$APP_ID'")')
-
-# Check if the window exists
-if [[ ! -z "$WINDOW_INFO" ]]; then
-    # Check if the window is visible
-    if [[ $(echo "$WINDOW_INFO" | jq '.visible') == "true" ]]; then
-        # If visible, hide the window
-        swaymsg "[app_id=\"$APP_ID\"] move scratchpad"
-    else
-        # If not visible, show the window
-        swaymsg "[app_id=\"$APP_ID\"] scratchpad show"
-    fi
+if pgrep -f "firefoxpwa.*01JJXZ3MN0JWBAR270M42K1QYG"; then
+    hyprctl dispatch togglespecialworkspace whatsapp
 else
-   firefoxpwa site launch 01HKQ02CZ0FJ90985QX9J9YGEK
+    firefoxpwa site launch 01JJXZ3MN0JWBAR270M42K1QYG
 fi
